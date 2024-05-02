fd02e1b301c97d368eb883dc5f6af926b36f3b90
shivaiisc <shivachandhraa@gmail.com>
2024-05-02 19:08:05 +0530

    shell_script

---
+++ run.sh
@@ -0,0 +1,11 @@
+git clone https://github.com/shivaiisc/EDGE_AI_.git
+
+cd EDGE_AI_
+
+conda create --name EDGE_AI_ python==3.9.19
+
+conda activate EDGE_AI_
+
+pip install requirements.txt
+
+python train.py
